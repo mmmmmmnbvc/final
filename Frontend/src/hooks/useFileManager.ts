ea795f4b5c203bf5790c0cb4fedc241986baadb7
mmmmmmnbvc <s6404062616012@kmutnb.ac.th>
2026-04-20 00:02:28 +0700

    first commit

--- Frontend/src/hooks/useFileManager.ts
+++ Frontend/src/hooks/useFileManager.ts
@@ -9,7 +9,7 @@ export interface FileItem {
   folder: string;
   url: string;
 }
-const BASE_URL = "https://notified-travelling-modules-lit.trycloudflare.com";
+const BASE_URL = "https://hiking-treated-elephant-lyrics.trycloudflare.com";
 
 const API = `${BASE_URL}/api/files`;
 const UPLOAD_API = `${BASE_URL}/api/upload`;
